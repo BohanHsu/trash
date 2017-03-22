ca62c8995b6b3921773cd799ec87b0c9c1195e4b
U-NETAPP\bohan <bohan@VIDHIB-PC.hq.netapp.com>
2017-03-22 15:32:44 -0400

    first commit

---
+++ Trash.py
@@ -0,0 +1,31 @@
+###############################################################################
+#
+# One util to replace the evil rm command
+# My intention is to write all functions to this single file.
+# Dev and test on python 2.7
+# 
+# For the first version, try use as much as python system API instead call '
+# system command.
+#
+###############################################################################
+
+###############################################################################
+# Import
+import os
+
+###############################################################################
+# GLOBAL VARIABLE
+DATA_DIR="~/.Trash"
+INSTALL_DIR=DATA_DIR + "/install"
+INSTALL_SCRIPT=INSTALL_DIR + "Trash.py"
+
+###############################################################################
+# Util functions
+
+def check_path_exist(path):
+
+
+###############################################################################
+# Installation
+
+
